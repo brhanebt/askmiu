1f6d19a4a8cc88d1e25545afd13c34cce3382763
efrem2019 <efrem2015@gmail.com>
2020-01-31 15:56:57 -0600

    added post question

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,35 @@
 const router = require("express").Router();
 const verify = require('./verifyToken');
+const Question=require('../models/Question');
+const ObjectId=require('mongodb').ObjectID;
+
 
 router.get('/',verify,(req,res)=>{
   
     res.json({message:"This is message"});
 })
+
+router.post("/addquestion", async (req, res) => {
+    
+    //create new 
+    const question = new Question({
+      postedby: new ObjectId(),
+      title: req.body.title,
+      date:new Date(),
+      body:req.body.body,
+    likes:0
+    });
+  
+    // Question.post()
+    try {
+      const savedQuestion = await question.save();
+      res.json({ question: savedQuestion });
+    } catch (err) {
+     console.log(err);
+        res.status({message:'Error Occured'});
+    }
+  });
+  
+
 
 module.exports = router;
