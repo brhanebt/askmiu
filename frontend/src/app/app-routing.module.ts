366fcd031f5b96c1837eba495926b4d6ae137b87
jeevan acharya <gvancharya@gmail.com>
2020-02-01 18:27:56 -0600

    Login And signup Page with cookies

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,13 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login/login.component';
 import { SignupComponent } from './components/signup/signup/signup.component';
 import { NavComponent } from './components/nav/nav/nav.component';
+import { HomeComponent } from './components/home/home/home.component';
 
 
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
+  {path: '', component: LoginComponent},
   {path: 'signup', component: SignupComponent},
-  {path: '*', component: NavComponent}
+  {path: '*', component: NavComponent},
+  {path: 'home', component: HomeComponent}
 
 
 ]
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,20 +7,30 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login/login.component';
 import { SignupComponent } from './components/signup/signup/signup.component';
 import { NavComponent } from './components/nav/nav/nav.component';
+import {MatButtonModule,MatSnackBarModule} from '@angular/material';
 
 import { MatSliderModule } from '@angular/material/slider';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Appconstant } from './utils/appconstant';
+import { CookieService } from 'ngx-cookie-service';
+import { Localcookie } from './utils/localcookie';
+import { HomeComponent } from './components/home/home/home.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignupComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
   ],
-  imports: [BrowserModule,HttpClientModule, AppRoutingModule, FormsModule, ReactiveFormsModule, MatSliderModule, BrowserAnimationsModule],
+  imports: [BrowserModule,HttpClientModule, AppRoutingModule, FormsModule,
+     ReactiveFormsModule, MatSliderModule, BrowserAnimationsModule,
+     MatButtonModule,MatSnackBarModule,
+    ],
 
-  providers: [],
+  providers: [Appconstant, CookieService,Localcookie],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
