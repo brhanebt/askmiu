4db4aea114cf60083203fbb2a85343439d215d19
jeevan acharya <gvancharya@gmail.com>
2020-02-04 17:18:45 -0600

    Master Cleaned

--- routes/posts.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const verify = require('./verifyToken');
-
-router.get('/',verify,(req,res)=>{
-  
-    res.json({message:"This is message"});
-})
-
-module.exports = router;
